f7b64ff6bf1ee375c79b02583ba97572b6d254c7
Didar Muktafa Billah <didarputra2007@gmail.com>
2025-08-30 22:59:46 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-from libs import welcome_message, exit
+from libs import welcome_message, exit_program
 from games import jeckpot
-from tools import warung
+from tools import keuangan
 
 def menu():
-    user_option = input('\nDAR...\n\n1. Games\n2. warung\n3. keluar')
+    user_option = input('\nDAR...\n\n1. Games\n2. Keuangan\n3. keluar')
     if user_option == '1':
      jeckpot.game()
     elif user_option == '2':
-       warung.start()
+       keuangan.start()
     elif user_option == '3':
-       exit()
+       exit_program()
     else:
        print('jan aneh2 ')
 
