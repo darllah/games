28b3c0a3e01da46860a117bc499bf25bf32e773b
Didar Muktafa Billah <didarputra2007@gmail.com>
2025-08-30 23:00:11 +0700

    Update libs.py

--- libs.py
+++ libs.py
@@ -7,7 +7,7 @@ def welcome_message():
     bintang = '*' * (len(PC_NAME) + 6)
     print(f'{bintang}\n** {PC_NAME} **\n{bintang}')
 
-def exit():
+def exit_program():
     print('program berhenti')
     sleep(1)
     print('3...')
@@ -17,6 +17,7 @@ def exit():
     print('1....')
     sleep(1)
     print('Selesai...')
+    exit()
 
 if __name__ == '__main__':
      welcome_message()
